5b6e16a2980de3521cf2e2f8fc3d7600c97abb76
Kasama <ralegro@bol.com.br>
2019-04-17 17:03:45 -0300

    Update deis version

--- HomebrewFormula/deis-tfg.rb
+++ HomebrewFormula/deis-tfg.rb
@@ -1,17 +1,17 @@
 class DeisTfg < Formula
   desc "The CLI for Deis Workflow"
   homepage "https://www.github.com/topfreegames/workflow-cli"
-  url "https://github.com/topfreegames/workflow-cli/archive/v2.20.0.tar.gz"
-  sha256 "ed5a6335c5ed292782deddebcc5e98a0fbc22f03f4619e3a4737ba7a85ae061d"
-  version "2.20.0"
+  url "https://github.com/topfreegames/workflow-cli/archive/v2.20.1.tar.gz"
+  sha256 "2763a3bcf00f53d4f15e5f65115098b5036d94f3e36a87a66d8e59b69c748b69"
+  version "2.20.1"
 
   depends_on "glide" => :build
   depends_on "go" => :build
 
   bottle do
-    root_url "https://github.com/topfreegames/workflow-cli/releases/download/v2.20.0"
+    root_url "https://github.com/topfreegames/workflow-cli/releases/download/v2.20.1"
     cellar :any_skip_relocation
-    sha256 "e1f8b3d5814770ee493d5cbec2f60aabc80710ebab1200857a1f6be99294277b" => :mojave
+    sha256 "ea4b6c0b37b22f2171ba8230af88acd287df8a32fea16e402006717dea27dfcb" => :mojave
   end
 
   def install
--- version/version.go
+++ version/version.go
@@ -1,4 +1,4 @@
 package version
 
 // Version identifies the Deis product revision.
-var Version = "v2.19.1"
+var Version = "v2.20.1"
